330f4561e5d0c4780955397320ea58c1ac123c4e
Illia Gavryliuk <illya.gavryliuk@unicorn.com>
2021-10-06 11:49:48 +0300

    Passed 8th lesson of spring security course.

--- src/main/java/com/havrulyk/springsecurityc6/config/ProjectConfig.java
+++ src/main/java/com/havrulyk/springsecurityc6/config/ProjectConfig.java
@@ -1,24 +1,26 @@
 package com.havrulyk.springsecurityc6.config;
 
-import com.havrulyk.springsecurityc6.entity.User;
-import com.havrulyk.springsecurityc6.security.authentication.TokenAuthentication;
 import com.havrulyk.springsecurityc6.security.filter.TokenAuthFilter;
 import com.havrulyk.springsecurityc6.security.filter.UsernamePasswordAuthFilter;
 import com.havrulyk.springsecurityc6.security.providers.OtpAuthenticationProvider;
 import com.havrulyk.springsecurityc6.security.providers.TokenAuthProvider;
 import com.havrulyk.springsecurityc6.security.providers.UsernamePasswordAuthenticationProvider;
+import org.springframework.beans.factory.InitializingBean;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 
 @Configuration
+//@EnableAsync
 public class ProjectConfig extends WebSecurityConfigurerAdapter {
 
   @Autowired
@@ -65,5 +67,14 @@ public class ProjectConfig extends WebSecurityConfigurerAdapter {
   @Bean
   public AuthenticationManager authenticationManagerBean() throws Exception {
     return super.authenticationManagerBean();
+  }
+
+  //InitializingBean is called by spring when initializing a context, so we can set instructions for executing before initialization of context
+  public InitializingBean initializingBean(){
+    return () -> {
+      //This strategy is for asynchronous processing of requests, because when @Async for controller is enabled, then it will have different threads for methods and information will be lost,
+      // this strategy inherits information from previous thread and stores it to current thread
+      SecurityContextHolder.setStrategyName(SecurityContextHolder.MODE_INHERITABLETHREADLOCAL);
+    };
   }
 }
--- src/main/java/com/havrulyk/springsecurityc6/controller/HelloController.java
+++ src/main/java/com/havrulyk/springsecurityc6/controller/HelloController.java
@@ -1,5 +1,9 @@
 package com.havrulyk.springsecurityc6.controller;
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,7 +12,23 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
 
   @RequestMapping(value = "/hello", method = RequestMethod.GET)
-  private String hello() {
+  //    @Async
+  public String hello() {
+    Runnable r = () -> {
+      Authentication authentication =
+          SecurityContextHolder.getContext().getAuthentication();
+
+      System.out.println(authentication);
+    };
+//        DelegatingSecurityContextRunnable dr = new DelegatingSecurityContextRunnable(r);
+
+    ExecutorService service = Executors.newSingleThreadExecutor();
+//        DelegatingSecurityContextExecutorService dService =
+//                new DelegatingSecurityContextExecutorService(service);
+
+    service.submit(r);
+    service.shutdown();
+
     return "Hello!";
-  }
+  }// token
 }
